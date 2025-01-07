874f450770b7ca239d78e6b0358a54ceecf1243a
Shawn Lovelidge <github@lernendercorp.com>
2025-01-07 10:51:38 -0600

    commit

--- projects/core/src/lib/common/style.ts
+++ projects/core/src/lib/common/style.ts
@@ -1,3 +1,5 @@
+import * as Library from '../library/index';
+
 export class Style {
   public height: string | number | undefined;
   public width: string | number | undefined;
@@ -6,6 +8,15 @@ export class Style {
   public font: string  | undefined;
   public fontSize: string  | undefined;
 
+  //
+  // Public Methods
+  //
+  public hasHeight = (): boolean => Library.isDefined(this.height);
+  public hasWidth = (): boolean => Library.isDefined(this.width);
+  public hasColor = (): boolean => Library.isDefined(this.color);
+  public hasBackgroundColor = (): boolean => Library.isDefined(this.backgroundColor);
+  public hasFont = (): boolean => Library.isDefined(this.font);
+  public hasFontSize = (): boolean => Library.isDefined(this.fontSize);
 
   constructor() {
     this.height = undefined;
